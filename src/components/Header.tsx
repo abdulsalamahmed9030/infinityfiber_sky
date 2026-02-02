b4826bde48656d890ba0f2050615caf2f4504bc8
abdulsalamahmed9030 <abdulsalamahmed9030@gmail.com>
2026-02-02 21:24:58 +0530

    sx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
       `}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16 md:h-20">
+          <div className="flex  justify-between h-16 md:h-20">
             {/* Logo */}
             <a href="/" className="flex items-center gap-2 overflow-hidden">
               <div className="relative overflow-hidden">
